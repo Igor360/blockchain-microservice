3a8b01b641d8a1fd31e18a21e99e6fad5a9ae234
Igor360 <igor.dawidencko@gmail.com>
2020-12-15 11:20:08 +0200

    Fix api file error

--- src/config/contract.js
+++ src/config/contract.js
@@ -1,5 +1,5 @@
 module.exports = {
     ownerAddress: process.env.OWNER_ADDRESS || '0xaF0bEC0FE0e81aB05D61F1720Fdd7092B617A64c',
-    address: process.env.CONTRACT_ADDRESS || '0x',
+    address: process.env.CONTRACT_ADDRESS || '0x8d03a219084Fd866cE7ed552A6ff804fCbFaB56C',
     abi: require('./ethereum/contract.abi.json') || null,
 };
